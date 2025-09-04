c3d163ecac889e342f55e16cc4fca40e43fa8b4f
Carter <cmatties2003@gmail.com>
2025-09-04 14:19:54 -0400

    Add code to run simulation

--- assign2/cow.py
+++ assign2/cow.py
@@ -1,3 +1,17 @@
+while r[1]>0:
+    F = get_force(v) #Change this to whatever Jada calls it
+    r_new, v_new = position_velocity_update(r, v, F, dt)
+    KE_new, PE_new, E_new = get_energies(r, v) #Change this to whatever Will calls it
+    
+    history["r"] = history["r"].append(r_new) #change 'history' to whatever Jada calls it
+    history["v"] = history["v"].append(v_new)
+    history["E"] = history["E"].append(E_new)
+    
+    r = r_new
+    v = v_new
+    t = t+dt
+
+
 def position_velocity_update(r, v, F, dt):
     a = F/m
     v_new = v + a*dt
