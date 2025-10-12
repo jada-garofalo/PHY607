7e8a2f684d0bb669f4a9031c69e3af4614604b0a
will-344 <willmartin344@gmail.com>
2025-10-12 19:44:42 -0400

    added notes

--- proj2/systemClass.py
+++ proj2/systemClass.py
@@ -9,15 +9,23 @@ class systemClass:
         self.total_time = total_time
         self.time_step = time_step
     
-    def integrate():
+    def accelerations(self):
+    
+        temp = 0
+    
+    def integrate(self):
         """
         this method should integrate the odes for all bodies across a timestep,
-        setting the updated trajectories
+        setting the updated trajectories.
+        
+        probably use rk4:
+        x_new = x + dt/6 * (k1x + 2*k2x + 2*k3x + k4x)
+        u_new = u + dt/6 * (k1u + 2*k2u + 2*k3u + k4u)
         """
         temp = 0
     
     
-    def interactions():
+    def interactions(self):
         """
         this method should determine the interaction type for any and all
         bodies within interaction distance, and compute and set the updated
