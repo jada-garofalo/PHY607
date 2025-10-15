b1e933de2be94b1a25f7387cc81da557e821e024
will-344 <willmartin344@gmail.com>
2025-10-15 18:39:11 -0400

    added interaction_detection method

--- proj2/main.py
+++ proj2/main.py
@@ -21,6 +21,7 @@ total_time = 10000
 time_step = 0.1
 gravity_constant = 6.6743 * 10**(-11) 
 dimensions = 2 # 2 or 3 for 2D or 3D motion
+interaction_distance = 0.001
 
 # choose limits for body mass and initial conditions
 mass_lim = np.array([0.1, 1])*100
@@ -28,7 +29,8 @@ position0_lim = np.array([-1.0, 1.0])*1
 velocity0_lim = np.array([-1.0, 1.0])*0.0001
 
 # create system with the above parameters
-n_body_system = System(n_bodies, total_time, time_step, gravity_constant, dimensions)
+n_body_system = System(n_bodies, total_time, time_step, gravity_constant, 
+                       dimensions, interaction_distance)
 
 # create array of bodies with random values for 
 bodies = np.array([])
@@ -38,22 +40,20 @@ for i in range(n_bodies):
     velocity = np.random.uniform(velocity0_lim[0], velocity0_lim[1], dimensions)
     bodies = np.append(bodies, Body(mass, position, velocity))
 
-print("v0", bodies[0].velocity)
-
 # integrate and update values
 iterations = round(total_time/time_step)
 for i in range(iterations):
     x_new, v_new = n_body_system.integrate(bodies)
-    for j in range(len(bodies)):
+    for j in range(n_bodies):
         bodies[j].update_state(x_new[j,:], v_new[j,:])
-
-print("vf", bodies[0].velocity)
+    interaction_list = n_body_system.interaction_detection(bodies)
+    
 
 # time the code
 end_time = timer.time()
 print("runtime:", end_time-start_time)
 
 # analyze...
-#system_analysis = Analysis(bodies)
-#system_analysis.plot_trajectories()
+system_analysis = Analysis(bodies)
+system_analysis.plot_trajectories()
 
--- proj2/systemClass.py
+++ proj2/systemClass.py
@@ -5,26 +5,25 @@ class System:
     this class contains the functions needed for the overall system
     """
     
-    def __init__(self, n_bodies, total_time, time_step, gravity_constant, dimensions):
+    def __init__(self, n_bodies, total_time, time_step, gravity_constant, 
+                 dimensions, interaction_distance):
         # system properties
         self.n_bodies = n_bodies
         self.total_time = total_time
         self.time_step = time_step
         self.G = gravity_constant
         self.dim = dimensions
+        self.interaction_distance = interaction_distance
     
-    def accelerations(self, bodies):
+    def accelerations_solver(self, bodies):
         """
         this method finds the acceleration of each body due to gravity
         forces from all other bodies
         a_i = G * sum( m_j * (r_j-r_i) / (r_j-r_i)^3 )
         """
-        positions = [bodies[0].position]
-        masses = np.array([bodies[0].mass])
-        accelerations = np.zeros((self.n_bodies,self.dim))
-        for i in range(self.n_bodies-1):
-            positions = np.append(positions, [bodies[i+1].position],axis=0)
-            masses = np.append(masses, [bodies[i+1].mass], axis=0)
+        
+        accelerations = np.zeros((self.n_bodies, self.dim))
+        positions, masses = self.position_mass_list(bodies)
         for i in range(self.n_bodies):
             distances = positions - positions[i]
             distances_cubed = np.array([np.sum(distances**2,1)**1.5]).T
@@ -33,33 +32,170 @@ class System:
                                                distances_cubed)
         return accelerations
     
+    def position_mass_list(self, bodies):
+        # returns lists of positions and masses in a math-ready format
+        positions = np.array([bodies[0].position])
+        masses = np.array([bodies[0].mass])
+        for i in range(self.n_bodies-1):
+            positions = np.append(positions, [bodies[i+1].position], axis=0)
+            masses = np.append(masses, [bodies[i+1].mass], axis=0)
+        return positions, masses
+    
     def integrate(self, bodies):
         """
-        this method should integrate the odes for all bodies across a timestep,
-        setting the updated trajectories.
-        
-        probably use rk4:
-        x_new = x + dt/6 * (k1x + 2*k2x + 2*k3x + k4x)
-        v_new = v + dt/6 * (k1v + 2*k2v + 2*k3v + k4v)
+        this method numerically integrates the odes for all bodies across a 
+        time step, giving the updated trajectories. (Euler's Method)
         """
-        a = self.accelerations(bodies)
-        #print("a", a)
-        v = np.zeros((self.n_bodies,self.dim))
-        x = np.zeros((self.n_bodies,self.dim))
+        
+        a = self.accelerations_solver(bodies)
+        v = np.zeros((self.n_bodies, self.dim))
+        x = np.zeros((self.n_bodies, self.dim))
         for i in range(self.n_bodies):
             v[i,:] = bodies[i].velocity
             x[i,:] = bodies[i].position
         v_new = v + a*self.time_step
-        #print("v", v_new)
         x_new = x + v*self.time_step
         
         return x_new, v_new
     
+    def interaction_detection(self, bodies):
+        """
+        this method checks for interaction between bodies, and is intended to
+        be used every iteration of the time loop.
+        
+        the output is a matrix containing which bodies are interacting, and is
+        split up into rows for each interaction. The numbers in the matrix are
+        the body numbers, where -1 means no body.
+        """
+        interaction_list = np.zeros((round(self.n_bodies/2),self.n_bodies))-1
+        positions, _ = self.position_mass_list(bodies)
+        for i in range(self.n_bodies):
+            distance_vectors = positions - positions[i]
+            distance_magnitudes = np.array([np.sum(distance_vectors**2,axis=1)**0.5]).T
+            
+            # temp list of interacting bodies
+            temp_interact_list = np.where(distance_magnitudes < 
+                                          self.interaction_distance)[0]
+                                          
+            # check if there are interactions
+            if len(temp_interact_list) > 1:
+                print(1)
+                # ^ interaction threshold was met (more than one body listed)
+                
+                # check if interacting bodies are listed in interaction_list
+                if np.any(interaction_list > -1) == False:
+                    # ^ no entries in list, add a row with the interacting bodies
+                    interaction_list[0,0:len(temp_interact_list)] = temp_interact_list
+                    
+                else:
+                    # ^ there are entries in interaction_list already
+                    
+                    # check if any bodies arent already in interaction_list
+                    mask1 = np.isin(temp_interact_list, interaction_list)
+                    # check if the listed bodies are all in the same row
+                    mask2 = np.isin(interaction_list, temp_interact_list)
+                    temp_list1 = np.sum(mask2, axis=1)
+                    if np.any(mask1==False) == False:
+                        # ^ all current bodies are already in the list
+                        
+                        if np.sum(temp_list1 > 0) != 1:
+                            # ^ they are not all in the same row
+                            
+                            # combine rows
+                            temp_list2 = np.any(mask2, axis=1)
+                            rows_to_combine = np.where(temp_list2)[0]
+                            # make a temp array with the entries from each row that needs to be combined
+                            temp_list3 = np.array([])
+                            for j in range(len(rows_to_combine)):
+                                mask = interaction_list[rows_to_combine[j],:] > -1
+                                temp_list3 = np.append(temp_list3, interaction_list[rows_to_combine[j],:][mask])
+                            
+                            # set the first row from rows_to_combine to the contents of temp_list3
+                            interaction_list[rows_to_combine[0],0:len(temp_list3)] = temp_list3
+                            # then clear the other rows that are from temp_list3
+                            interaction_list[rows_to_combine[1:],:] = -1
+                        
+                    else:
+                        # ^ not all current bodies are in the list
+                        
+                        if np.sum(temp_list1 > 0) == 1:
+                            # ^ they are all in the same row
+                            
+                            # add missing entries to that row:
+                            # get an array of entries that need to be added to the row
+                            entries_to_append = temp_interact_list[~mask1]
+                            
+                            # find which row is getting added to
+                            row_to_append = np.where(mask2 == True)[0][0]
+                            
+                            # get the index of the first -1 in the row
+                            append_index = np.where(interaction_list[row_to_append,:]==-1)[0][0]
+                            
+                            # insert the entries at that index
+                            interaction_list[row_to_append,append_index:len(entries_to_append)] = entries_to_append
+                            
+                        else:
+                            # ^ they are not all in the same row
+                            
+                            # combine rows, then add missing entries to that row:
+                            # combine rows
+                            temp_list2 = np.any(mask2, axis=1)
+                            rows_to_combine = np.where(temp_list2)[0]
+                            # make a temp array with the entries from each row that needs to be combined
+                            temp_list3 = np.array([])
+                            for j in range(len(rows_to_combine)):
+                                mask = interaction_list[rows_to_combine[j],:] > -1
+                                temp_list3 = np.append(temp_list3, interaction_list[rows_to_combine[j],:][mask])
+                            # set the first row from rows_to_combine to the contents of temp_list3
+                            interaction_list[rows_to_combine[0],0:len(temp_list3)] = temp_list3
+                            # then clear the other rows that are from temp_list3
+                            interaction_list[rows_to_combine[1:],:] = -1
+                            
+                            # then add missing entries to that row:
+                            # rewrite mask2 with updated interaction_list
+                            mask2 = np.isin(interaction_list, temp_interact_list)
+                            # get an array of entries that need to be added to the row
+                            entries_to_append = temp_interact_list[~mask1]
+                            
+                            # find which row is getting added to
+                            row_to_append = np.where(mask2 == True)[0][0]
+                            
+                            # get the index of the first -1 in the row
+                            append_index = np.where(interaction_list[row_to_append,:]==-1)[0][0]
+                            
+                            # insert the entries at that index
+                            interaction_list[row_to_append,append_index:len(entries_to_append)] = entries_to_append        
+        return interaction_list
     
-    def interactions(self):
+    def interactions(self, bodies):
         """
         this method should determine the interaction type for any and all
         bodies within interaction distance, and compute and set the updated
         trajectories and masses for those bodies
         """
-        temp = 0
+        
+            
+            
+        # something to choose which interaction method is used
+        
+        
+        n_interacting_bodies = len(interacting_bodies)
+        
+        if interation_type == 1:
+            # fully plastic interaction
+            # m1*v1 + m2*v2 = (m1+m2)*v3
+            mass = 0
+            momentum = 0
+            for i in range(n_interacting_bodies):
+                mass += interacting_bodies[i].mass
+                momentum += (interacting_bodies[i].mass *
+                             interacting_bodies[i].velocity)
+            # write over first interacting body
+            
+            
+        elif interaction_type == 2:
+            # partial plastic interaction
+            1
+        else:
+            # fully elastic interaction
+            1
