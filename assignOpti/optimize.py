cc60edb0324e8785eb65cd04869083257057241a
jada-garofalo <jada.garofalo@gmail.com>
2025-10-23 11:47:43 -0400

    add brute force func

--- assignOpti/optimize.py
+++ assignOpti/optimize.py
@@ -7,8 +7,21 @@ y = numpy.arange(-1, 3, .01)
 X, Y = numpy.meshgrid(x, x)
 z = rosen((X, Y))
 
-def brute_force():
-    #smth
+#example bounds = [(-2,2),(-1,3)]
+
+def brute_force(func, bounds, step=0.01):
+    grids = [np.arange(b[0], b[1], step) for b in bounds] #for given bounds, makes arrays of all points per dimension to evaluate
+    mesh = np.meshgrid(*grids) #sorts points into a mesh
+    points = np.stack([m.flatten() for m in mesh], axis=1) #flattens mesh into a discrete list to go through
+
+    best_val = np.inf
+    best_point = None
+    for p in points:
+        val = func(p)
+        if val < best_val:
+            best_val = val
+            best_point = p
+    return np.array(best_point), best_val
 
 def grad_desc():
     #smth
