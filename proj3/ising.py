307244977636e9584bb08171a877988e08cd0b27
will-344 <willmartin344@gmail.com>
2025-11-15 12:35:55 -0500

    added setup section to ising.py

--- proj3/ising.py
+++ proj3/ising.py
@@ -11,4 +11,13 @@ J = 1
 k = 1
 T = 1
 
+# SETUP ----------------------------------------------------------------------
+
+# initial spin, random
+s = np.zeros((Ly, Lx, nIter+1))
+s[:,:,0] = np.random.randint(-1, 1, (Ly,Lx))
+s[:,:,0][s[:,:,0]==0] = 1
+
+beta = 1/(k*T)
+
 
