bdcf38350ad48c7ce89015bce7bd5c7c8e119b96
will-344 <willmartin344@gmail.com>
2025-11-15 12:35:02 -0500

    start the ising mcmc

---
+++ proj3/ising.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# USER INPUT -----------------------------------------------------------------
+
+Lx = 10 # number of x positions
+Ly = 20 # number of y positions
+
+nIter = 100000 # number of iterations
+
+J = 1
+k = 1
+T = 1
+
+
