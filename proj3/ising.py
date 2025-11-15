9fa971ea90f2da5cad42a15ecdfdb7a7da59266d
will-344 <willmartin344@gmail.com>
2025-11-15 12:36:29 -0500

    added solver to ising.py

--- proj3/ising.py
+++ proj3/ising.py
@@ -20,4 +20,38 @@ s[:,:,0][s[:,:,0]==0] = 1
 
 beta = 1/(k*T)
 
+# SOLVER ---------------------------------------------------------------------
 
+for n in range(nIter):
+    # fill in next page of spin matrix
+    s[:,:,n+1] = s[:,:,n]
+   
+    # pick a spin site randomly
+    i = np.random.randint(0, Lx)
+    j = np.random.randint(0, Ly)
+   
+    # calculate change in energy with the flipped spin 
+    # (wrap around lattice boundaries)
+    if i == 0:
+        sx_sum = s[j,i+1,n] + s[j,Lx-1,n]
+    elif i == Lx-1:
+        sx_sum = s[j,0,n] + s[j,i-1,n]
+    else:
+        sx_sum = s[j,i+1,n] + s[j,i-1,n]
+    if j == 0:
+        sy_sum = s[j+1,i,n] + s[Ly-1,i,n]
+    elif j == Ly-1:
+        sy_sum = s[0,i,n] + s[j-1,i,n]
+    else:
+        sy_sum = s[j+1,i,n] + s[j-1,i,n]
+       
+    dE = 2*s[j,i,n]*J*(sx_sum+sy_sum)
+   
+    # if the change in energy is not positive, keep the flipped spin
+    if dE <= 0:
+        s[j,i,n+1] = -s[j,i,n]
+    # if the change in energy is positive, keep with probability exp(-beta*dE)
+    elif np.random.rand() < np.exp(-beta*dE):
+        s[j,i,n+1] = -s[j,i,n]
+           
+    # repeat
