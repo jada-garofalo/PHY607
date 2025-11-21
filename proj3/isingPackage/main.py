180b0277fea1f53dc06f470d2967bdb2d6c8647b
jada-garofalo <jada.garofalo@gmail.com>
2025-11-21 10:10:35 -0500

    fix plotting stuff

--- proj3/isingPackage/main.py
+++ proj3/isingPackage/main.py
@@ -1,13 +1,13 @@
 import numpy as np
-from isingClass import ising
-from analysisClass import analysis
+from .isingClass import ising
+from .analysisClass import analysis
 import matplotlib.pyplot as plt
 
 # USER INPUT -------------------------
 Lx = 10
 Ly = 10
 nIter = 50000
-J = 1
+J_arr = [0.5, 1] # various J to test
 k = 1
 T = 1
 # ------------------------------------
@@ -33,55 +33,75 @@ def gelman_rubin(chains):
 # 1. HANDWRITTEN MCMC CHAIN
 # -------------------------------------------------
 
-ising_2D = ising(Lx, Ly, nIter, J, k, T)
-an = analysis()
+for J in J_arr:
+    ising_2D = ising(Lx, Ly, nIter, J, k, T)
+    an = analysis()
 
-chain1 = ising_2D.mcmc_hand_written()
-chain2 = ising_2D.mcmc_hand_written()
+    chain1 = ising_2D.mcmc_hand_written()
+    chain2 = ising_2D.mcmc_hand_written()
 
-mag1 = ising_2D.Magnetization(chain1)
-mag2 = ising_2D.Magnetization(chain2)
+    mag1 = ising_2D.Magnetization(chain1)
+    mag2 = ising_2D.Magnetization(chain2)
 
-energy1 = ising_2D.Energy(chain1)
-energy2 = ising_2D.Energy(chain2)
+    energy1 = ising_2D.Energy(chain1)
+    energy2 = ising_2D.Energy(chain2)
 
-tau1, _ = an.ACL(mag1)
-tau2, _ = an.ACL(mag2)
+    tau1, _ = an.ACL(mag1)
+    tau2, _ = an.ACL(mag2)
 
-rhat_mag = gelman_rubin([mag1, mag2])
+    rhat_mag = gelman_rubin([mag1, mag2])
 
-print("=======================================")
-print(" HANDWRITTEN METROPOLIS RESULTS")
-print("=======================================")
-print(f"ACL Chain 1 = {tau1:.3f}")
-print(f"ACL Chain 2 = {tau2:.3f}")
-print(f"R-hat (magnetization) = {rhat_mag:.5f}")
-print()
+    print("=======================================")
+    print(" HANDWRITTEN METROPOLIS RESULTS")
+    print("=======================================")
+    print(f"ACL Chain 1 = {tau1:.3f}")
+    print(f"ACL Chain 2 = {tau2:.3f}")
+    print(f"R-hat (magnetization) = {rhat_mag:.5f}")
+    print()
 
-# plots -------------------------------------------
-fig1 = plt.figure(1)
-ax1 = fig1.add_subplot(1, 1, 1)
-fig2 = plt.figure(2)
-ax2 = fig2.add_subplot(1, 1, 1)
-fig3 = plt.figure(3)
-ax3 = fig3.add_subplot(1, 1, 1)
-fig4 = plt.figure(4)
-ax4 = fig4.add_subplot(1, 1, 1)
-for n in range(10):
-    s_n = ising_2D.mcmc_hand_written()
-    mag_n = ising_2D.Magnetization(s_n)
-    E_n = ising_2D.Energy(s_n)
-    _, rho_mag_n = an.ACL(mag_n)
-    _, rho_E_n = an.ACL(E_n)
-    ax1.plot(np.linspace(0,len(mag_n),len(mag_n)),mag_n)
-    ax2.plot(np.linspace(0,len(E_n),len(E_n)),E_n)
-    ax3.plot(np.linspace(0,len(rho_mag_n),len(rho_mag_n)),rho_mag_n)
-    ax4.plot(np.linspace(0,len(rho_E_n),len(rho_E_n)),rho_E_n)
-ax1.set_title("trace plot of magnetization")
-ax2.set_title("trace plot of energy")
-ax3.set_title("ACF of magnetization")
-ax4.set_title("ACF of energy")
-plt.show()
+    # plots -------------------------------------------
+    fig1 = plt.figure(1)
+    ax1 = fig1.add_subplot(1, 1, 1)
+    fig2 = plt.figure(2)
+    ax2 = fig2.add_subplot(1, 1, 1)
+    fig3 = plt.figure(3)
+    ax3 = fig3.add_subplot(1, 1, 1)
+    fig4 = plt.figure(4)
+    ax4 = fig4.add_subplot(1, 1, 1)
+    for n in range(10):
+        s_n = ising_2D.mcmc_hand_written()
+        mag_n = ising_2D.Magnetization(s_n)
+        E_n = ising_2D.Energy(s_n)
+        _, rho_mag_n = an.ACL(mag_n)
+        _, rho_E_n = an.ACL(E_n)
+        ax1.plot(np.linspace(0,len(mag_n),len(mag_n)),mag_n)
+        ax2.plot(np.linspace(0,len(E_n),len(E_n)),E_n)
+        ax3.plot(np.linspace(0,len(rho_mag_n),len(rho_mag_n)),rho_mag_n)
+        ax4.plot(np.linspace(0,len(rho_E_n),len(rho_E_n)),rho_E_n)
+    ax1.set_title(f"Trace plot of magnetization for J={J}")
+    ax1.set_xlabel("Iteration")
+    ax1.set_ylabel("Magnetization")
+    ax2.set_title(f"Trace plot of energy for J={J}")
+    ax2.set_xlabel("Iteration")
+    ax2.set_ylabel("Energy")
+    ax3.set_title(f"ACF of magnetization for J={J}")
+    ax3.set_xlabel("Lag")
+    ax3.set_ylabel("Autocorrelation")
+    ax4.set_title(f"ACF of energy for J={J}")
+    ax4.set_xlabel("Lag")
+    ax4.set_ylabel("Autocorrelation")
+    plt.show()
+    if J < 1:
+        J = 10 * J # file names no decimals
+    fig1.savefig(f"trace_magnetization_J{J}.png")
+    fig2.savefig(f"trace_energy_J{J}.png")
+    fig3.savefig(f"acf_magnetization_J{J}.png")
+    fig4.savefig(f"acf_energy_J{J}.png")
+    plt.close(fig1)
+    plt.close(fig2)
+    plt.close(fig3)
+    plt.close(fig4)
+J = 1
 
 # -------------------------------------------------
 # 2. PACKAGE COMPARISON SAMPLER
@@ -129,4 +149,6 @@ print("=======================================")
 print(f"Mean J = {np.mean(J_samples):.3f}")
 print(f"ACL (J) = {tau_J:.3f}")
 print(f"R-hat(J) = {rhat_J:.5f}")
+
+print("END PROJECT SCRIPT")
 
