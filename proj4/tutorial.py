4c8703b30a0a8592a8b2d2ae4c83310963e73a8d
Carter <cmatties2003@gmail.com>
2025-12-09 13:35:07 -0500

    Solution animation code

--- proj4/tutorial.py
+++ proj4/tutorial.py
@@ -1,2 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+
+# Animate solution
+
+x = # numpy array of x-values, shape (N,) for N spatial points
+t = # Array of t-values, shape (M,) for M time points
+w = # Array of w(x,t). Shape (M,N) for N spatial points & M time points
+
+fig, ax = plt.subplots()
+line = ax.plot(x, w[0,:], color = "C0")[0]
+points = ax.scatter(x,w[0,:], color = "C0")
+ax.set(xlim = (0, 2*np.pi), ylim = [-1.1,1.1])
+
+
+def update(frame):
+    data = np.stack([x, w[frame,:]]).T
+    points.set_offsets(data)
+    
+    ax.set_title(f"T = {t[frame]}")
+    line.set_ydata(w[frame,:])
+    return (points, line)
+    
+ani = animation.FuncAnimation(fig = fig, func = update, frames = len(t), interval = 30)
+plt.show()
